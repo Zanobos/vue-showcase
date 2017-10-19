251d8d331798dc8c9707cf7a13f6095e2e8b0a41
Andrea Zanotti <a.zanotti@reply.it>
2017-10-19 10:53:13 +0200

    avanzamento grafico

--- src/components/IndicatoriPortafoglio.vue
+++ src/components/IndicatoriPortafoglio.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="indicatori-portafoglio">
     <h3>Indicatori Portafoglio Cliente</h3>
-    <line-chart></line-chart>
+    <line-chart :data="datacollection"></line-chart>
   </div>
 </template>
 
@@ -12,6 +12,25 @@ export default {
   name: 'IndicatoriPortafoglio',
   components: {
     LineChart
+  },
+  data() {
+    return {
+      datacollection: {
+        //Data to be represented on x-axis
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f3f3f3',
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#249EBF',
+            //Data to be represented on y-axis
+            data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+          }
+        ]
+      }
+    }
   }
 }
 </script>
