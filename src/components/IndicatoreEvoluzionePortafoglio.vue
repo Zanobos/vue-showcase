8b48734add2a0d36a8cf70f789444139fd10a014
Andrea Zanotti <a.zanotti@reply.it>
2017-10-27 11:29:28 +0200

    avanzamento hp

---
+++ src/components/IndicatoreEvoluzionePortafoglio.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="evoluzione">
+        <h3>Indicatore Evoluzione Portafoglio</h3>
+        <line-chart :data="evoluzionePortafoglio"></line-chart>
+    </div>
+</template>
+
+<script>
+import LineChart from "@/components/charts/LineChart"
+
+export default {
+    name: 'IndicatoreEvoluzionePortafoglio',
+    components: {
+        LineChart
+    },
+    props: ['evoluzionePortafoglio']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-        <h1>{{ msg }}</h1>
-        <indicatori-portafoglio/>
-        <profilatura-cliente :cliente="cliente"/>
+        <indicatore-evoluzione-portafoglio :evoluzionePortafoglio="evoluzionePortafoglio" class="mt-3"/>
+        <profilatura-cliente :cliente="cliente" class="mt-3"/>
     </div>
 </template>
 
 <script>
-import IndicatoriPortafoglio from "@/components/IndicatoriPortafoglio.vue";
+import IndicatoreEvoluzionePortafoglio from "@/components/IndicatoreEvoluzionePortafoglio.vue";
 import ProfilaturaCliente from "@/components/ProfilaturaCliente.vue";
 
 const cliente= {
@@ -33,16 +32,32 @@ const cliente= {
     ]
 };
 
+const evoluzionePortafoglio= {
+    //Data to be represented on x-axis
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
+    datasets: [
+        {
+            label: 'Evoluzione Portafoglio',
+            backgroundColor: '#f3f3f3',
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#249EBF',
+            //Data to be represented on y-axis
+            data: [14000, 13020, 15030, 16050, 17790, 18110, 17920, 16440, 18950, 19270, 20190, 21100]
+        }
+    ]
+};
+
 export default {
     name: 'HomePage',
     components: {
-        IndicatoriPortafoglio,
+        IndicatoreEvoluzionePortafoglio,
         ProfilaturaCliente
     },
     data () {
         return {
-            msg: 'Questa è l\'home page',
-            cliente: cliente
+            cliente: cliente,
+            evoluzionePortafoglio: evoluzionePortafoglio
         }
     }
 }
