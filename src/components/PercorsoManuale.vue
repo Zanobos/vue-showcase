62753fb199f958af7a138cbb1d79677e60a49171
Andrea Zanotti <a.zanotti@reply.it>
2017-10-30 12:48:10 +0100

    Binding con i gruppi dinamico

--- src/components/PercorsoManuale.vue
+++ src/components/PercorsoManuale.vue
@@ -1,67 +1,95 @@
 <template>
     <div>
         <tabella-simulazione 
-                :items="posizioniRiserva" 
-                :title="titleRiserva">         
+            v-for="gruppo in input.gruppi" 
+            :key="gruppo.codice"
+            :items="gruppo.posizioni" 
+            :title="gruppo.titolo">  
         </tabella-simulazione>
 
-        <tabella-simulazione 
-                :items="posizioniInvestimento" 
-                :title="titleInvestimento">         
-        </tabella-simulazione>
+        <div><strong>Totale Patrimonio </strong>{{totalePatrimonio}}</div>
     </div>
 </template>
 
 <script>
 import TabellaSimulazione from '@/components/TabellaSimulazione.vue'
 
-const posizioniRiserva = [
-    {   codiceProdotto: '-',
-        descrizione: 'CONTO CORRENTE',
-        rischio: '-',
-        ctvIniziale: 25000.45,
-        acquista: null,
-        vendi: null,
-        ctvFinale: null
-    },
-    {   codiceProdotto: 'IT6892341',
-        descrizione: 'BTP 12/56 M-ITA',
-        rischio: '2.5',
-        ctvIniziale: 4003.23,
-        acquista: null,
-        vendi: null,
-        ctvFinale: null
-    }
-];
-
-const posizioniInvestimento = [
-    {   codiceProdotto: 'IT3248822',
-        descrizione: 'ENEL',
-        rischio: '9.12',
-        ctvIniziale: 13259.14,
-        acquista: null,
-        vendi: null,
-        ctvFinale: null
-    },
-    {   codiceProdotto: 'NL123999',
-        descrizione: 'CED ATTIVA PIU',
-        rischio: '4.5',
-        ctvIniziale: 20091.62,
-        acquista: null,
-        vendi: null,
-        ctvFinale: null
-    },
-    {   codiceProdotto: 'XYZ234922',
-        descrizione: 'ISP PROSPETTIVA',
-        rischio: '1.2',
-        ctvIniziale: 150091.4,
-        acquista: null,
-        vendi: null,
-        ctvFinale: null
-    }
-];
-
-
+const input = {
+    gruppi: [
+        {
+            codice: 'SPESA',
+            titolo: 'Spesa',
+            posizioni: [
+                {   
+                    codiceProdotto: '-',
+                    descrizione: 'CONTO CORRENTE',
+                    rischio: '-',
+                    ctvIniziale: 3000.0,
+                    acquista: null,
+                    vendi: null,
+                    ctvFinale: null
+                }
+            ]
+        },
+        {
+            codice: 'OBI10',
+            titolo: 'Riserva',
+            posizioni: [
+                {   
+                    codiceProdotto: '-',
+                    descrizione: 'CONTO CORRENTE',
+                    rischio: '-',
+                    ctvIniziale: 25000.45,
+                    acquista: null,
+                    vendi: null,
+                    ctvFinale: null
+                },
+                {   
+                    codiceProdotto: 'IT6892341',
+                    descrizione: 'BTP 12/56 M-ITA',
+                    rischio: '2.5',
+                    ctvIniziale: 4003.23,
+                    acquista: null,
+                    vendi: null,
+                    ctvFinale: null
+                }
+            ]
+        },
+        {
+            codice: 'OBI20',
+            titolo: 'Investimento',
+            posizioni: [
+                {   
+                    codiceProdotto: 'IT3248822',
+                    descrizione: 'ENEL',
+                    rischio: '9.12',
+                    ctvIniziale: 13259.14,
+                    acquista: null,
+                    vendi: null,
+                    ctvFinale: null
+                },
+                {  
+                    codiceProdotto: 'NL123999',
+                    descrizione: 'CED ATTIVA PIU',
+                    rischio: '4.5',
+                    ctvIniziale: 20091.62,
+                    acquista: null,
+                    vendi: null,
+                    ctvFinale: null
+                },
+                {   
+                    codiceProdotto: 'XYZ234922',
+                    descrizione: 'ISP PROSPETTIVA',
+                    rischio: '1.2',
+                    ctvIniziale: 150091.4,
+                    acquista: null,
+                    vendi: null,
+                    ctvFinale: null
+                }
+            ]
+        }
+    ]
+}
 
 export default {
     name: 'PercorsoManuale',
@@ -70,10 +98,8 @@ export default {
     },
     data () {
         return {
-            posizioniRiserva: posizioniRiserva,
-            posizioniInvestimento: posizioniInvestimento,
-            titleInvestimento: "Investimento",
-            titleRiserva: "Riserva"
+            input: input,
+            totalePatrimonio: 0
         }
     }
 }
